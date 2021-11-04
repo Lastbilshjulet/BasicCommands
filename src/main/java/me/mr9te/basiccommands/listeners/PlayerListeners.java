47d9e7b82fdf0c897a5ba05db68a9cd08e362704
Johan Fredriksson <johan.9te@gmail.com>
2021-11-04 21:07:31 +0100

    Added scary sound when sleeping

--- src/main/java/me/mr9te/basiccommands/listeners/PlayerListeners.java
+++ src/main/java/me/mr9te/basiccommands/listeners/PlayerListeners.java
@@ -3,13 +3,11 @@ package me.mr9te.basiccommands.listeners;
 import me.mr9te.basiccommands.BasicCommands;
 import org.bukkit.ChatColor;
 import org.bukkit.Sound;
+import org.bukkit.SoundCategory;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerBedEnterEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
-import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.event.player.*;
 import org.bukkit.plugin.Plugin;
 
 public class PlayerListeners implements Listener {
@@ -39,6 +37,13 @@ public class PlayerListeners implements Listener {
         Player player = event.getPlayer();
         if (event.getBedEnterResult() == PlayerBedEnterEvent.BedEnterResult.OK) {
             plugin.getServer().broadcastMessage(ChatColor.GOLD + player.getDisplayName() + ": " + ChatColor.DARK_GRAY + "Dirt.");
+            player.playSound(player.getLocation(), Sound.MUSIC_DISC_13, SoundCategory.MUSIC, (float) 1.0, (float) 1.0);
         }
+    }
+
+    @EventHandler
+    public void onExitBed(PlayerBedLeaveEvent event) {
+        Player player = event.getPlayer();
+        player.stopSound(Sound.MUSIC_DISC_13, SoundCategory.MUSIC);
     }
 }
